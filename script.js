cf475480f7530636a3bde3b72e4cb2dae208aa24
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-03-31 15:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
-  { id: 4, name: "Product 4", price: 40 },
-  { id: 5, name: "Product 5", price: 50 },
+  { 'id': 1, 'name': "Product 1", 'price': 10 },
+  { 'id': 2, 'name': "Product 2", 'price': 20 },
+  { 'id': 3, 'name': "Product 3", 'price': 30 },
+  { 'id': 4, 'name': "Product 4", 'price': 40 },
+  { 'id': 5, 'name': "Product 5", 'price': 50 },
 ];
 
 const productList = document.getElementById("product-list");
